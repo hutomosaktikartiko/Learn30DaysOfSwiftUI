05e664a4dd1871242664094d43cc68dc2fb6e5a0
Hutomo Sakti Kartiko <hutomosaktikartiko.dev@gmail.com>
2024-03-10 17:06:01 +0700

    Initial Commit

---
+++ Learn30DaysOfSwiftUI/Learn30DaysOfSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn30DaysOfSwiftUIApp.swift
+//  Learn30DaysOfSwiftUI
+//
+//  Created by Hutomo on 10/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Learn30DaysOfSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
