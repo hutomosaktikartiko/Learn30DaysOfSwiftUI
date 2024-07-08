26b458b53f8296a448b3cfbc01e64479e38cd76a
Hutomo Sakti Kartiko <hutomosaktikartiko.dev@gmail.com>
2024-07-08 16:59:33 +0700

    Formatting

---
+++ Learn30DaysOfSwiftUI/Day6.swift
@@ -0,0 +1,18 @@
+//
+//  Day6.swift
+//  Learn30DaysOfSwiftUI
+//
+//  Created by Hutomo on 08/07/24.
+//
+
+import SwiftUI
+
+struct Day6: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Day6()
+}
